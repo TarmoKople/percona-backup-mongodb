d2a7bb663757b9329e9823ae7a89551e48ab800a
Tarmo Kople <tarmo@ekiri.ee>
2022-12-30 11:52:52 +0200

    cleanup replicaset info

--- nftport/nftp.go
+++ nftport/nftp.go
@@ -134,22 +134,22 @@ func prepareData(bcp *pbm.BackupMeta) error {
 
 	ctx := context.Background()
 
-	//err = c.Database("local").Collection("replset.minvalid").Drop(ctx)
-	//if err != nil {
-	//	return errors.Wrap(err, "drop replset.minvalid")
-	//}
-	//err = c.Database("local").Collection("replset.oplogTruncateAfterPoint").Drop(ctx)
-	//if err != nil {
-	//	return errors.Wrap(err, "drop replset.oplogTruncateAfterPoint")
-	//}
-	//err = c.Database("local").Collection("replset.election").Drop(ctx)
-	//if err != nil {
-	//	return errors.Wrap(err, "drop replset.election")
-	//}
-	//_, err = c.Database("local").Collection("system.replset").DeleteMany(ctx, bson.D{})
-	//if err != nil {
-	//	return errors.Wrap(err, "delete from system.replset")
-	//}
+	err = c.Database("local").Collection("replset.minvalid").Drop(ctx)
+	if err != nil {
+		return errors.Wrap(err, "drop replset.minvalid")
+	}
+	err = c.Database("local").Collection("replset.oplogTruncateAfterPoint").Drop(ctx)
+	if err != nil {
+		return errors.Wrap(err, "drop replset.oplogTruncateAfterPoint")
+	}
+	err = c.Database("local").Collection("replset.election").Drop(ctx)
+	if err != nil {
+		return errors.Wrap(err, "drop replset.election")
+	}
+	_, err = c.Database("local").Collection("system.replset").DeleteMany(ctx, bson.D{})
+	if err != nil {
+		return errors.Wrap(err, "delete from system.replset")
+	}
 
 	_, err = c.Database("local").Collection("replset.minvalid").InsertOne(ctx,
 		bson.M{"_id": primitive.NewObjectID(), "t": -1, "ts": primitive.Timestamp{0, 1}},
